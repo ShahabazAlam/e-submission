717daa253fd7b24ab38a1fb5c6b9f7b6644bddaf
Shahabaz Alam <70678880+ShahabazAlam@users.noreply.github.com>
2021-08-07 17:52:42 +0530

    Update settings.py

--- assignment_submission_system/settings.py
+++ assignment_submission_system/settings.py
@@ -158,9 +158,9 @@ MEDIA_URL = '/media/'
 # SMTP CONFIGURATION
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_HOST = ''
+EMAIL_PORT = 
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'patelvraj7143@gmail.com'
-EMAIL_HOST_PASSWORD = 'gguukzlvqcigsgrx'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
